1fb307a7268c5488177744993db832bd39487ce2
Jimmy Beugnies <93620368+Zrtehhz@users.noreply.github.com>
2023-11-05 14:28:49 +0100

    .

--- player/player.js
+++ player/player.js
@@ -56,7 +56,7 @@ function initializePlayer(videoId) {
     videoId: videoId,
 
     playerVars: {
-      'autoplay': 1,
+      'autoplay': 0,
       'controls': 0,
       'playsinline': 1
     },
@@ -85,4 +85,65 @@ function toggleHeart(element) {
     element.classList.toggle('filled'); // Basculer la classe pour changer l'apparence du cœur
     if (element.classList.contains('filled')) {
     }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var playPauseButton = document.querySelector('.play-pause-icon');
+  var currentTimeElement = document.querySelector('.current-time');
+  var totalTimeElement = document.querySelector('.total-time');
+  var isPlaying = false;
+
+  // Mise à jour initiale du temps total (sera mis à jour après le chargement de la vidéo)
+  totalTimeElement.textContent = '0:00';
+
+  // Gestionnaire de clic pour le bouton de lecture/pause
+  playPauseButton.addEventListener('click', function() {
+    if (youtubePlayer && youtubePlayer.getPlayerState() === YT.PlayerState.PLAYING) {
+      youtubePlayer.pauseVideo();
+      playPauseButton.setAttribute('d', 'M18 12L0 24V0'); // SVG path for 'play' icon
+      isPlaying = false;
+    } else if (youtubePlayer) {
+      youtubePlayer.playVideo();
+      playPauseButton.setAttribute('d', 'M0 0h6v24H0zM12 0h6v24h-6z'); // SVG path for 'pause' icon
+      isPlaying = true;
+    }
+  });
+
+  // Mise à jour de l'interface utilisateur avec l'état actuel de la vidéo
+  function updatePlayerUI() {
+    if (youtubePlayer) {
+      var currentTime = youtubePlayer.getCurrentTime();
+      var duration = youtubePlayer.getDuration();
+      currentTimeElement.textContent = formatTime(currentTime);
+      totalTimeElement.textContent = formatTime(duration);
+      // Mise à jour de la barre de progression si nécessaire
+    }
+  }
+
+  // Fonction pour formater le temps en minutes:secondes
+  function formatTime(timeInSeconds) {
+    if (!isNaN(timeInSeconds)) {
+      var minutes = Math.floor(timeInSeconds / 60);
+      var seconds = Math.floor(timeInSeconds % 60);
+      return minutes + ':' + (seconds < 10 ? '0' + seconds : seconds);
+    }
+    return '0:00';
+  }
+
+  // Mettre à jour l'UI régulièrement
+  setInterval(updatePlayerUI, 1000);
+});
+
+// Plus besoin de ce code qui était destiné au fakeAudio
+// ...
+
+// Intégrez ceci dans votre fonction onPlayerReady
+function onPlayerReady(event) {
+  // La mise à jour initiale du temps total doit être ici
+  var duration = youtubePlayer.getDuration();
+  document.querySelector('.total-time').textContent = formatTime(duration);
+
+  // Plus besoin d'ajouter des gestionnaires d'événements ici,
+  // car nous avons déjà ajouté l'écouteur d'événements ci-dessus
 }
